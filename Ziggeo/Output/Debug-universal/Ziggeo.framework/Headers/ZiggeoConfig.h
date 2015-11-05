c3390da6a9cc1da625a93c4c287069e7756edb12
alex <yats-alex@yandex.ru>
2015-11-06 00:49:33 +0300

    minor updates

---
+++ Ziggeo/Output/Debug-universal/Ziggeo.framework/Headers/ZiggeoConfig.h
@@ -0,0 +1,16 @@
+//
+//  ZiggeoConfig.h
+//
+//  Copyright (c) 2015 Ziggeo Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZiggeoConfig : NSObject
+
+@property (strong, nonatomic) NSString *server_api_url;
+@property (strong, nonatomic) NSString *cdn_api_url;
+
+- (id)init;
+
+@end
---
+++ Ziggeo/Output/Debug-universal/Ziggeo.framework/Headers/ZiggeoRecorder.h
@@ -0,0 +1,18 @@
+//
+//  ZiggeoRecorder.h
+//  Ziggeo
+//
+//  Copyright (c) 2015 Ziggeo Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ZiggeoApplication.h"
+#import "CoverSelectorController.h"
+@import UIKit;
+
+@interface ZiggeoRecorder : UIImagePickerController<UIImagePickerControllerDelegate,UINavigationControllerDelegate,CoverSelectorDelegate>
+
+-(id) initWithZiggeoApplication:(Ziggeo*)ziggeo;
+@property (nonatomic) bool coverSelectorEnabled;
+
+@end
