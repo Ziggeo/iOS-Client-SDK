8f03fbc4a1fafc444de5ce28dabe8788b9b4415d
Severyn <severyn@ziggeo.com>
2023-09-26 22:01:07 -0600

    added cache config

---
+++ Frameworks/ZiggeoMediaSDK.framework/Headers/RecorderInterfaceConfig.h
@@ -0,0 +1,20 @@
+//
+//  RecorderInterfaceConfig.h
+//  ZiggeoMediaSDK
+//
+//  Created by dragon on 11/15/18.
+//  Copyright © 2016 Ziggeo Inc. All rights reserved.
+//
+
+
+#import <Foundation/Foundation.h>
+
+@class ButtonConfig;
+
+@interface RecorderInterfaceConfig : NSObject
+
+@property (nonatomic) ButtonConfig *recordButton;
+@property (nonatomic) ButtonConfig *closeButton;
+@property (nonatomic) ButtonConfig *cameraFlipButton;
+
+@end
